3ecb3b1c40df4c8a7067827f14bef4f3ff2acea5
OlivierRecher <recher.olivier@outlook.fr>
2025-05-19 16:56:52 +0200

    feat(#2): rework eslint and prettier to work with express

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,cjs,mjs,ts}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+      parser: tseslint.parser,
+    },
+    plugins: {
+      js,
+      '@typescript-eslint': tseslint.plugin,
+      prettier: pluginPrettier,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      'prettier/prettier': 'error',
+    },
+  },
+
+  {
+    files: ['**/*.{ts,js}'],
+    rules: {
+      ...prettier.rules,
+    },
+  },
+]);
--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,9 +1,9 @@
 type configType = {
-    [key : string] : string|number
-}
+  [key: string]: string | number;
+};
 
-const config : configType = {
-    "PORT" : 3001,
-}
+const config: configType = {
+  PORT: 3001,
+};
 
-export {config}
+export { config };
