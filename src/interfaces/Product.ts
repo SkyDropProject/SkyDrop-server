26d9935f9d25e549279a90dab748aad81f06d68b
ianb1204 <ian.bertin2004@gmail.com>
2025-06-05 12:22:03 +0200

    feat(#30): Product controllers + routers

--- src/interfaces/Product.ts
+++ src/interfaces/Product.ts
@@ -2,11 +2,13 @@ import { CategoryType } from './Category';
 
 interface ProductType {
   _id?: string;
+  imageUrl:string;
   name: string;
   price: number;
   description: string;
   stock: number;
   weight: number;
+  star:boolean;
   categoryId: CategoryType | string;
 }
 
--- src/routers/ProductRouter.ts
+++ src/routers/ProductRouter.ts
@@ -1,6 +1,22 @@
 import express, { Request, Response, Router } from 'express';
 import { auth } from './../utils/auth';
 import { ProductController } from '../controllers/ProductController';
+import multer from 'multer';
+import path from 'path';
+import { config } from '../utils/config';
+
+const storage = multer.diskStorage({
+  destination: (req:Request, file:any, cb:Function) => {
+    cb(null, config.uploadPath)
+  },
+  filename: (req:Request, file:any, cb:Function) => {
+    const ext = path.extname(file.originalname)
+    const baseName = Date.now() + '-' + Math.round(Math.random() * 1e9)
+    cb(null, baseName + ext)
+  }
+})
+
+const upload = multer({ storage: storage })
 
 class ProductRouter {
   public router: Router;
@@ -13,10 +29,10 @@ class ProductRouter {
       .get(auth.authenticate(), async (req: Request, res: Response) => {
         await productController.find(req, res);
       })
-      .put(auth.authenticate(), async (req: Request, res: Response) => {
+      .put(auth.authenticate(), upload.single("image"), async (req: Request, res: Response) => {
         await productController.insert(req, res);
       })
-      .post(auth.authenticate(), async (req: Request, res: Response) => {
+      .post(auth.authenticate(), upload.single("image"), async (req: Request, res: Response) => {
         await productController.update(req, res);
       });
 
@@ -28,6 +44,12 @@ class ProductRouter {
       .delete(auth.authenticate(), async (req: Request, res: Response) => {
         await productController.delete(req, res);
       });
+
+    this.router
+      .route('/star')
+      .get(auth.authenticate(), async (req: Request, res: Response) => {
+        await productController.getStar(req, res);
+      })
   }
 }
 
