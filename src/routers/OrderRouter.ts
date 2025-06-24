bb7a3415b618e9e246ff5eb0bdb706c64a103a46
ianb1204 <ian.bertin2004@gmail.com>
2025-06-24 14:58:28 +0200

    feat(#72): Route de completion de commande /order/completed/:id + message

--- src/routers/OrderRouter.ts
+++ src/routers/OrderRouter.ts
@@ -24,6 +24,10 @@ class OrderRouter {
       await orderController.findAll(req, res);
     });
     this.router
+      .route('/completed/:id').get(auth.authenticate(), async (req: any, res: Response) => {
+      await orderController.completed(req, res);
+    });
+    this.router
       .route('/:id')
       .get(auth.authenticate(), async (req: Request, res: Response) => {
         await orderController.findOne(req, res);
